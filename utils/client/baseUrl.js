c13578cb000313504043ba63750139eedaa2df1d
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2021-11-25 21:56:06 +0530

    Changes to deploy the web app

--- utils/client/baseUrl.js
+++ utils/client/baseUrl.js
@@ -1,3 +1,6 @@
-const baseUrl = "http://localhost:3000";
+const baseUrl =
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:3000"
+    : "https://connectify-socialmedia.herokuapp.com";
 
 export default baseUrl;
