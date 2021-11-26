7f67d9c484e04d096206d3881cbcc686c3e65bdb
Avash_Mitra <63412949+Avash027@users.noreply.github.com>
2021-11-27 00:51:36 +0530

    Fixing upload image bug

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  env: {
+    CLOUDINARY_URL: `https://api.cloudinary.com/v1_1/dxdpj87op/image/upload`,
+  },
+  pwa: {
+    dest: "public",
+    register: true,
+    sw: "/sw.js",
+  },
+});
